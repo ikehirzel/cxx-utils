8073aa12372ab19bedb68584e251a26c8ec4d965
Ike Hirzel <hirzelisaac@gmail.com>
2020-10-16 02:24:23 -0700

    Changed name of functions to avoid naming conflicts

--- src/fountain.h
+++ src/fountain.h
@@ -17,17 +17,17 @@ namespace hirzel
 	
 }
 
-#define info(msg)			hirzel::log_push(FOUNTAIN_INFO, __FILE__, __LINE__, msg, NULL)
-#define infof(msg, ...)		hirzel::log_push(FOUNTAIN_INFO, __FILE__, __LINE__, msg, __VA_ARGS__)
+#define log_info(msg) hirzel::log_push(FOUNTAIN_INFO, __FILE__, __LINE__, msg, NULL)
+#define log_infof(msg, ...) hirzel::log_push(FOUNTAIN_INFO, __FILE__, __LINE__, msg, __VA_ARGS__)
 
-#define success(msg)		hirzel::log_push(FOUNTAIN_SUCCESS, __FILE__, __LINE__, msg, NULL)
-#define successf(msg, ...)	hirzel::log_push(FOUNTAIN_SUCCESS, __FILE__, __LINE__, msg, __VA_ARGS__)
+#define log_success(msg) hirzel::log_push(FOUNTAIN_SUCCESS, __FILE__, __LINE__, msg, NULL)
+#define log_successf(msg, ...) hirzel::log_push(FOUNTAIN_SUCCESS, __FILE__, __LINE__, msg, __VA_ARGS__)
 
-#define warning(msg)		hirzel::log_push(FOUNTAIN_WARNING, __FILE__, __LINE__, msg, NULL)
-#define warningf(msg, ...)	hirzel::log_push(FOUNTAIN_WARNING, __FILE__, __LINE__, msg, __VA_ARGS__)
+#define log_warning(msg) hirzel::log_push(FOUNTAIN_WARNING, __FILE__, __LINE__, msg, NULL)
+#define log_warningf(msg, ...) hirzel::log_push(FOUNTAIN_WARNING, __FILE__, __LINE__, msg, __VA_ARGS__)
 
-#define error(msg)			hirzel::log_push(FOUNTAIN_ERROR, __FILE__, __LINE__, msg, NULL)
-#define errorf(msg, ...)	hirzel::log_push(FOUNTAIN_ERROR, __FILE__, __LINE__, msg, __VA_ARGS__)
+#define log_error(msg) hirzel::log_push(FOUNTAIN_ERROR, __FILE__, __LINE__, msg, NULL)
+#define log_errorf(msg, ...) hirzel::log_push(FOUNTAIN_ERROR, __FILE__, __LINE__, msg, __VA_ARGS__)
 
-#define fatal(msg)			hirzel::log_push(FOUNTAIN_FATAL, __FILE__, __LINE__, msg, NULL)
-#define fatalf(msg, ...)	hirzel::log_push(FOUNTAIN_FATAL, __FILE__, __LINE__, msg, __VA_ARGS__)
+#define log_fatal(msg) hirzel::log_push(FOUNTAIN_FATAL, __FILE__, __LINE__, msg, NULL)
+#define log_fatalf(msg, ...) hirzel::log_push(FOUNTAIN_FATAL, __FILE__, __LINE__, msg, __VA_ARGS__)
