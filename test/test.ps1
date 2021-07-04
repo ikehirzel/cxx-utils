5d78f35ea64a02e14917e7fadbc4199960a8a1b1
Isaac Hirzel <hirzelisaac@gmail.com>
2021-07-03 23:11:58 -0700

    Updated windows build file to not conflict with standard library.

--- test/test.ps1
+++ test/test.ps1
@@ -1,2 +1,2 @@
 clang++ obj.cpp -shared -o obj.dll
-clang++ main.cpp impl.cpp -o main.exe -Wall -Wextra -std=c++11 -pedantic -I../include && ./main
+clang++ main.cpp impl.cpp -o main.exe -pedantic -I../include && ./main
